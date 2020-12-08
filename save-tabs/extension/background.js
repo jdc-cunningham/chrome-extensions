dd1fd0d92ce0acb78e6de52e88d23f63d38d75ad
Jacob Cunningham <jcun4128@gmail.com>
2020-12-08 03:25:22 -0600

    working tab dump, need interface and external API

--- save-tabs/extension/background.js
+++ save-tabs/extension/background.js
@@ -4,4 +4,13 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
   });
+
+  // get tabs
+  chrome.tabs.query({}, function(tabs){
+    tabs.forEach(tb => {
+        // chrome.tabs.sendMessage(tb.id, { action: "xxx" });
+        console.log(tb.title, '\n');
+        console.log(tb.url);
+    });
+  });
 });
